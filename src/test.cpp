c421f366573710d8188b708ffd04ab3568749972
Shiv Upadhyay <shivnupadhyay@gmail.com>
2023-03-21 17:51:31 -0400

    add changes from discussion

--- src/test.cpp
+++ src/test.cpp
@@ -7,8 +7,12 @@ int main(int argc, char **argv) {
     Eigen::Matrix<double, 1, 10> row_vec = Eigen::Matrix<double, 1, 10>::Random();
 
     H5Easy::File file("example.h5", H5Easy::File::Overwrite);
-    H5Easy::dump(file, "/col_vec", col_vec);
-    H5Easy::dump(file, "/row_vec", row_vec);
+
+    H5Easy::dump(file, "col_vec_orig", col_vec);
+    H5Easy::dump(file, "row_vec_orig", row_vec);
+
+    file.createDataSet("row_vec", row_vec);
+    file.createDataSet("col_vec", col_vec);
 
     return 0;
 }
